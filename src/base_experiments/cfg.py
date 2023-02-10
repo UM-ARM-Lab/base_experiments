b7abd199a798bd98a10dc3b660080f345b3e3ad6
Sheng Zhong <zhsh@umich.edu>
2023-02-10 18:46:26 -0500

    Move the rest of cfg to base environment's cfg

--- src/base_experiments/cfg.py
+++ src/base_experiments/cfg.py
@@ -6,3 +6,15 @@ VIDEO_DIR = os.path.expanduser('~/experiments/video')
 URDF_DIR = os.path.expanduser('~/experiments/urdf')
 
 PIC_SAVE_DIR = os.path.expanduser('~/experiments/pics')
+
+ros_pkg_name = None
+
+
+def ensure_rviz_resource_path(filepath):
+    """Sanitize some path to something in this package to be RVIZ resource loader compatible"""
+    # get path after the first instance
+    if ros_pkg_name is None:
+        raise RuntimeError("ros_pkg_name must be set with base_experiments.fg.ros_pkg_name = 'name' "
+                           "to allow finding of resources")
+    relative_path = filepath.partition(ros_pkg_name)[2]
+    return f"package://{ros_pkg_name}/{relative_path.strip('/')}"
