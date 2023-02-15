f3262eade45f562034742a4bc7b6029a9bc69702
Sheng Zhong <zhsh@umich.edu>
2023-02-15 16:48:53 -0500

    Use base_experiments to store object models and make it a ros package

--- src/base_experiments/cfg.py
+++ src/base_experiments/cfg.py
@@ -3,11 +3,11 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 DATA_DIR = os.path.expanduser('~/experiments/data')
 VIDEO_DIR = os.path.expanduser('~/experiments/video')
-URDF_DIR = os.path.expanduser('~/experiments/urdf')
+URDF_DIR = os.path.join(ROOT_DIR, "urdf")
 
 PIC_SAVE_DIR = os.path.expanduser('~/experiments/pics')
 
-ros_pkg_name = None
+ros_pkg_name = "base_experiments"
 
 
 def ensure_rviz_resource_path(filepath):
