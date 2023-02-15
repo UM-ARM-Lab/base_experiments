8f93ef30ebfd6c23410d92b834b9106a13ffb8dd
Sheng Zhong <zhsh@umich.edu>
2023-02-15 14:23:34 -0500

    Factor out matrix to pose conversion to kinematics library

--- src/base_experiments/util.py
+++ src/base_experiments/util.py
@@ -1,5 +1,4 @@
 import matplotlib
-import pytorch_kinematics
 
 
 def move_figure(f, x, y):
@@ -13,11 +12,3 @@ def move_figure(f, x, y):
         # This works for QT and GTK
         # You can also use window.setGeometry
         f.canvas.manager.window.move(x, y)
-
-
-def matrix_to_pos_rot(m):
-    """Convert 4x4 transformation matrix to (position, xyzw quatnerion) used by pybullet and RViz"""
-    pos = m[:3, 3]
-    rot = pytorch_kinematics.matrix_to_quaternion(m[:3, :3])
-    rot = pytorch_kinematics.transforms.wxyz_to_xyzw(rot)
-    return pos, rot
