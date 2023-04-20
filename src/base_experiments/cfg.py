c396a7cc375dd45147a6ef94d2433c80e460563b
Sheng Zhong <zhsh@umich.edu>
2023-04-20 19:05:57 -0400

    Add mesh directory, a mug, and a desk mesh

--- src/base_experiments/cfg.py
+++ src/base_experiments/cfg.py
@@ -3,6 +3,7 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 DATA_DIR = os.path.expanduser('~/experiments/data')
 VIDEO_DIR = os.path.expanduser('~/experiments/video')
+MESH_DIR = os.path.join(ROOT_DIR, "meshes")
 URDF_DIR = os.path.join(ROOT_DIR, "urdf")
 LOG_DIR = os.path.expanduser('~/experiments/logs')
 
